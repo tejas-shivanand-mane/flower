3971ca10839b34b96e6406343d49100f35f38468
tejas <tmane002@ucr.edu>
2024-08-29 05:45:42 -0700

    fc

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-sudo apt install update
+sudo apt update
 sudo apt -y install build-essential
 sudo apt -y install git
 
 cd ../
 wget https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
     
-sh Anaconda3-2024.06-1-Linux-x86_64.sh -b
+sh Anaconda3-2024.06-1-Linux-x86_64.sh -b -u
 
 cd anaconda3/bin/
 echo 'export PATH=$PATH:$(pwd)' >> ~/.bashrc
