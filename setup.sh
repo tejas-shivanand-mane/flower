9b575a39662b3a9b47a97c2f61a459b0f1e11f58
tejas <tmane002@ucr.edu>
2024-08-29 06:01:52 -0700

    fc

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@ sudo apt update
 sudo apt -y install build-essential
 sudo apt -y install git
 
-cd
+cd /home/tejas
+
 wget https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
     
 sh Anaconda3-2024.06-1-Linux-x86_64.sh -b -u
