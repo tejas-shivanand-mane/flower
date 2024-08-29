4a200312bfd72c1e04f9caf01ce1133440746324
tejas <tmane002@ucr.edu>
2024-08-29 06:24:56 -0700

    fc

--- examples/pytorch-from-centralized-to-federated/server.py
+++ examples/pytorch-from-centralized-to-federated/server.py
@@ -21,7 +21,7 @@ strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_av
 
 # Start Flower server
 fl.server.start_server(
-    server_address="0.0.0.0:8080",
+    server_address="10.128.15.194:8080",
     config=fl.server.ServerConfig(num_rounds=10),
     strategy=strategy,
 )
--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ wget https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
     
 sh Anaconda3-2024.06-1-Linux-x86_64.sh -b -u -p /home/tejas/anaconda3
 
-export PATH=$PATH:/home/tejas/anaconda3/bin/ &&
+export PATH=$PATH:/home/tejas/anaconda3/bin/
 
 conda install -y pytorch torchvision torchaudio cpuonly -c pytorch
 
