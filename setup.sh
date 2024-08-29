6519bea7c8311b6059db29c346cd1bbba5bbb421
tejas <tmane002@ucr.edu>
2024-08-29 06:15:42 -0700

    fc

--- setup.sh
+++ setup.sh
@@ -8,10 +8,7 @@ wget https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
     
 sh Anaconda3-2024.06-1-Linux-x86_64.sh -b -u -p /home/tejas/anaconda3
 
-cd /home/tejas/anaconda3
-echo 'export PATH=$PATH:$(pwd)' >> ~/.bashrc
-
-source ~/.bashrc
+export PATH=$PATH:/home/tejas/anaconda3/bin/ &&
 
 conda install -y pytorch torchvision torchaudio cpuonly -c pytorch
 
